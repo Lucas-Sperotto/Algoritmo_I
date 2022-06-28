6528524af28e3fae1015af7ff794c492d81a2ced
Sperotto <sperotto.lucas@gmail.com>
2022-06-28 11:21:58 -0300

    Update Prog7.c

--- src/Aula_1/Prog7.c
+++ src/Aula_1/Prog7.c
@@ -1,4 +1,4 @@
-/* Prog7.cpp
+/* Prog7.c
  * Mostra o modificador unsigned
  */
 #include <stdio.h> /* para printf() */
@@ -10,8 +10,8 @@ int main()
     unsigned j = 4294967000UL;
     int i = j;
 
-    printf("\nVari·vel unsigned = %u", j);
-    printf("\nVari·vel int = %d\n", i);
+    printf("\nVari√°vel unsigned = %u", j);
+    printf("\nVari√°vel int = %d\n", i);
 
     system("PAUSE");
     return 0;
