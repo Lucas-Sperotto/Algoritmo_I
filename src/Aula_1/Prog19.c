31c20f5b0747b2cb0c95f90cdcb8c64c66cb4fe3
Sperotto <sperotto.lucas@gmail.com>
2022-07-29 21:59:36 -0300

    Update Prog19.c

--- src/Aula_1/Prog19.c
+++ src/Aula_1/Prog19.c
@@ -5,7 +5,7 @@
 //#include <locale.h> /* para setlocale() */
 int main()
 {
-    //setlocale(LC_ALL, "Portuguese"); /* Configura o idioma portugues */
+    //setlocale(LC_ALL, "portuguese-brazilian"); /* Configura o idioma portugues */
     system("cls");/*Limpa a tela*/
     printf("\n\n");
 
