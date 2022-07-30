b7f347d8980ee7dea3ba0144068d1518a6ee6580
Sperotto <sperotto.lucas@gmail.com>
2022-07-29 22:01:18 -0300

    Update Prog4.c

--- src/Aula_1/Prog4.c
+++ src/Aula_1/Prog4.c
@@ -1,8 +1,10 @@
 /* Prog4.c */
 #include <stdio.h> /* para printf() */
 #include <stdlib.h>/* para system() */
+#include <locale.h> /* para setlocale() */
 int main()
 {
+    setlocale(LC_ALL, "portuguese-brazilian"); /* Configura o idioma portugues */
     printf("A letra %c", 'J');
     printf(" pronuncia-se %s%c%c", "Jota", '.', '\n');
 
