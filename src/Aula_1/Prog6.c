966dd54240041e5e23f63ab62d65abed6368108d
Sperotto <sperotto.lucas@gmail.com>
2022-07-29 21:58:03 -0300

    Update Prog6.c

--- src/Aula_1/Prog6.c
+++ src/Aula_1/Prog6.c
@@ -1,18 +1,18 @@
 /*Prog6.c
- *Mostra inicializaÁ„o de vari·veis
+ *Mostra inicializa√ß√£o de vari√°veis
  */
 #include <stdio.h> /* para printf() */
 #include <stdlib.h> /* para system() */
 #include <locale.h> /* para setlocale() */
 int main()
 {
-    setlocale(LC_ALL, "Portuguese"); /* Configura o idioma portugues */
+    setlocale(LC_ALL, "portuguese-brazilian"); /* Configura o idioma portugues */
     int evento = 5;	/* define evento inicializada com 5     */
     char corrida = 'C'; /* define corrida inicializada com C  */
     float tempo = 27.25;/* define tempo inicializada com 27.25*/
 
-    printf("\nO tempo vitorioso na eliminatÛria %c", corrida);
-    printf("\nda competiÁ„o %d foi %f2.1.\n", evento, tempo);
+    printf("\nO tempo vitorioso na eliminat√≥ria %c", corrida);
+    printf("\nda competi√ß√£o %d foi %f2.1.\n", evento, tempo);
 
     system("PAUSE");
     return 0;
